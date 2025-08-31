7a03cf6ef15f63eba001f70122240065c10ea8e8
Samarth Uttampalle <suttampalle@gmail.com>
2025-08-31 19:48:17 +0530

    todo app final

--- todo_main/views.py
+++ todo_main/views.py
@@ -4,7 +4,11 @@ from todo.models import Task
 
 def home(request):
     tasks = Task.objects.filter(is_completed=False).order_by('-upadetd_at')
+    
+    completed_tasks = Task.objects.filter(is_completed=True).order_by('-upadetd_at')
+    
     context = {
-        'tasks': tasks
+        'tasks': tasks,
+        'completed_tasks': completed_tasks,
     }
     return render(request, 'home.html', context)
